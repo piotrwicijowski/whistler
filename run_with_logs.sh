b9d100b64bb7050e7647c2498a86665ac7541e29
Piotr Wicijowski <piotr.wicijowski@gmail.com>
2017-05-22 21:32:47 +0200

    Improved stdout and stderr log tee'ing

--- run_with_logs.sh
+++ run_with_logs.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 NOW=$(date +"%Y-%m-%d_%H-%M-%S")
-python2 $@ > ${NOW}_log.txt 2> ${NOW}_err.txt
+# python2 $@ > ${NOW}_log.txt 2> ${NOW}_err.txt
+python2 $@ > >(tee -a ${NOW}_log.txt) 2> >(tee -a ${NOW}_err.txt >&2)
